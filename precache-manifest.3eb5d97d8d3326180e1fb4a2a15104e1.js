aae6d0b6a7e077ed72a48f6dcaf1c6a7fb73720d
SherKhan <gurovg@gmail.com>
2020-09-16 01:28:40 +0300

    adds bcs-frontend-challenge

---
+++ precache-manifest.3eb5d97d8d3326180e1fb4a2a15104e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fb838ab34179d90d7c14bf98d2131c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cbbd7e70c376931d909",
+    "url": "/static/css/main.1ef59cdc.chunk.css"
+  },
+  {
+    "revision": "c8461e26d45054efc41c",
+    "url": "/static/js/2.38f037fe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.38f037fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbbd7e70c376931d909",
+    "url": "/static/js/main.66177d48.chunk.js"
+  },
+  {
+    "revision": "de8fbe0c887b435c6c7d",
+    "url": "/static/js/runtime-main.e3ef3a96.js"
+  },
+  {
+    "revision": "81504e2e9c60d78922f674320e7a759a",
+    "url": "/static/media/SuisseIntl-Medium.81504e2e.woff2"
+  },
+  {
+    "revision": "8f1f742094fb0f3f388760a6d47888cd",
+    "url": "/static/media/SuisseIntl-Regular.8f1f7420.woff2"
+  }
+]);
